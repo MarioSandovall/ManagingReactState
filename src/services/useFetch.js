4e8c9965204bfda6481746b20508a7a62533d8f1
Mario <mario.sandoval@freeagentsoftware.com>
2021-02-06 13:59:00 -0600

    Managing state via Refs

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoadig] = useState(true);
 
   useEffect(() => {
+    isMounted.current = true;
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if(isMounted) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if(isMounted) setError(e);
       } finally {
-        setLoadig(false);
+        if(isMounted) setLoadig(false);
       }
     }
     init();
+
+    return () => {
+      isMounted.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
